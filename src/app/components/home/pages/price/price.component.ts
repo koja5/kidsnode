fbaec42d5ce776c2217cfcee7af10df3d45d0925
koja5 <kojaaa95@gmail.com>
2022-04-06 00:33:15 +0200

    Working on landing pages

--- src/app/components/home/pages/price/price.component.ts
+++ src/app/components/home/pages/price/price.component.ts
@@ -3,13 +3,42 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-price',
   templateUrl: './price.component.html',
-  styleUrls: ['./price.component.scss']
+  styleUrls: ['./price.component.scss'],
 })
 export class PriceComponent implements OnInit {
+  public prices = [
+    {
+      count: '0 - 50',
+      price: '40',
+    },
+    {
+      count: '51 - 100',
+      price: '65',
+    },
+    {
+      count: '101 - 200',
+      price: '110',
+    },
+    {
+      count: '201 - 350',
+      price: '170',
+    },
+    {
+      count: '351 - 500',
+      price: '210',
+    },
+    {
+      count: '500 - ~',
+      price: '240',
+    },
+  ];
+  public selectedPackage = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  selectPackage(index: number) {
+    this.selectedPackage = index;
   }
-
 }
